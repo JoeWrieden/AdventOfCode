0d4395a89c3d4d69203f51ebd33f9a6d69b4e40d
Joe <37711916+JoeWrieden@users.noreply.github.com>
2019-12-14 13:53:21 +0000

    Upload Day 14

---
+++ Day14/day14_pt1.py
@@ -0,0 +1,40 @@
+import math
+def loadTable():
+    table = {}
+    useNum = {}
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            inp, output  = line.strip().split(" => ")
+            count, out = output.split()
+            temp = []
+
+            for x in inp.split(", "):
+                inCount, inVal = x.split()
+                if inVal not in useNum:
+                    useNum[inVal] = 0
+                useNum[inVal] += 1
+
+                temp.append((int(inCount), inVal))
+            table[out] = (int(count), temp)
+    return table, useNum
+
+def calculateUsage(toFind):
+    table, useNum = loadTable()
+    useNum["FUEL"] = 0
+    need = {"FUEL":1}
+    while len(useNum) > 1:
+        for element in useNum:
+            if useNum[element] == 0:
+                numberNeeded = need[element]
+                count, inputs = table[element]
+                amountToCreate = math.ceil(numberNeeded/count)
+                for toCount, toVal in inputs:
+                    if toVal not in need:
+                        need[toVal] = 0
+                    need[toVal] += toCount * amountToCreate
+                    useNum[toVal] -= 1
+                del useNum[element]
+                break
+    return need[toFind]
+
+print(calculateUsage("ORE"))
